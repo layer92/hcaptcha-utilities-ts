3a284d08b8a8d0f3b78250469971e175b035202f
layer92 <user@example.com>
2024-05-18 10:50:46 +0900

    removed incorrect assumptions about captcha secret key format

--- HCaptchaSecretBox.ts
+++ HCaptchaSecretBox.ts
@@ -3,8 +3,9 @@ import { Box, OnException, Expect } from "@layer92/core";
 /** A key used to authenticate the server. */
 export class HCaptchaSecretBox extends Box<string>{
     constructor(data:string,onValidationFail?:OnException){
-        Expect(data.length===42,"length must be OnException");
-        Expect(data.startsWith("0x"), "must start with 0x");
+        // these assumptions are incorrect
+        // Expect(data.length===42,"length must be 42");
+        // Expect(data.startsWith("0x"), "must start with 0x");
         super(data);
     }
 }
